bb2eb4113a2a8a545956f0ca7d95376dee990396
Aman Bokade <amanbokade1312@gmail.com>
2023-10-19 19:03:27 +0530

    update

--- git.py
+++ git.py
@@ -1,12 +1,14 @@
-import os , random
+import os
+import random
 
-for i in range(10):
+
+for i in range(20):
     d = str(i) + 'days ago'
-    rand = random.randrange(1, 6)
+    rand = random.randrange(1, 12)
     with open('test.txt','a') as file:
-        file.write(d+'\n')
-    os.system('git add test.txt')
-    os.system('git commit --date=" 2023-'+str(rand)+'-'+d+'" -m 1')
+        file.write(d)
+    os.system('git add .')
+    os.system('git commit --date=" 2023-'+str(rand)+'-'+d+'" -m "update"')
 os.system('git push -u origin main')
 
 #git commit --amend --no-edit --date="Fri Nov 6 20:00:00 2015 -0600" 
